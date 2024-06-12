64dcf53a4209b1391f9768d6ec45ccbcc8f91fad
Jari de Keijzer <j.n.dekeijzer@student.tudelft.nl>
2024-06-12 17:45:03 +0200

    Project clean-up, NL

--- app/(client)/not-found.tsx
+++ app/(client)/not-found.tsx
@@ -3,9 +3,9 @@
 export default function NotFoundPage() {
   return (
     <main className="flex min-h-screen w-full flex-col items-center p-8">
-      <h1 className="text-lg font-bold">Not Found</h1>
+      <h1 className="text-lg font-bold">Pagina niet gevonden</h1>
 
-      <p className="text-sm">Could not find requested resource</p>
+      <h1 className="text-lg font-bold">Page not found</h1>
     </main>
   );
 }
