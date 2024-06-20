5fdca58a316b23d09d5c4b1c62acefec91c55567
Jari de Keijzer <j.n.dekeijzer@student.tudelft.nl>
2024-06-20 13:36:41 +0200

    Only show dutch opening statement, for now

--- pages/index.vue
+++ pages/index.vue
@@ -6,13 +6,13 @@
   <main class="flex min-h-screen w-full flex-col items-center p-8">
     <h1 class="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl">
       Contemporary Creativity:
-      <small class="ms-2 font-semibold text-gray-500 dark:text-gray-400">The Many Faces of AI Art</small>
+      <small class="ms-2 font-semibold text-gray-500">The Many Faces of AI Art</small>
     </h1>
-    <p class="mb-6 text-lg font-normal text-gray-500 dark:text-gray-400 sm:px-16 lg:text-xl xl:px-48">
+    <p class="mb-6 text-lg font-normal text-gray-500 sm:px-16 lg:text-xl xl:px-48">
       Een onderzoek naar de creatieve potentie van AI gegenereerde afbeeldingen.
     </p>
-    <OpeningNL class="mb-3" />
-    <OpeningEN />
+    <OpeningNL />
+    <!--    <OpeningEN /> -->
   </main>
 </template>
 
