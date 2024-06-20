d5c4aff7a78d8582f11a2287ada84f237537355a
Jari de Keijzer <j.n.dekeijzer@student.tudelft.nl>
2024-06-20 13:37:40 +0200

    Made navbar active to path, also included nuxt links

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -7,45 +7,59 @@ import { FwbNavbar, FwbNavbarCollapse, FwbNavbarLink, FwbNavbarLogo } from 'flow
 <template>
   <fwb-navbar>
     <template #logo>
-      <fwb-navbar-logo
-        alt="TU Delft - Delft University of Technology - logo"
-        image-url="/TU-Delft/logo-descriptor-png/TUDelft_logo_descriptor_rgb.png"
-        link="/"
-      >
-        Contemporary Creativity
-      </fwb-navbar-logo>
+      <NuxtLink to="/">
+        <fwb-navbar-logo
+          alt="TU Delft - Delft University of Technology - logo"
+          image-url="/TU-Delft/logo-descriptor-png/TUDelft_logo_descriptor_rgb.png"
+        >
+          Contemporary Creativity
+        </fwb-navbar-logo>
+      </NuxtLink>
     </template>
     <template #default="{ isShowMenu }">
       <fwb-navbar-collapse :is-show-menu="isShowMenu">
-        <fwb-navbar-link
-          :is-active="$route.name === '/'"
-          link="/"
-        >
-          Home
+        <fwb-navbar-link>
+          <NuxtLink
+            to="/"
+            active-class="text-TUcyan"
+          >
+            Home
+          </NuxtLink>
         </fwb-navbar-link>
-        <fwb-navbar-link
-          :is-active="$route.name === '/explanation'"
-          link="/explanation"
-        >
-          Uitleg
+        <fwb-navbar-link>
+          <NuxtLink
+            to="/explanation"
+            active-class="text-TUcyan"
+          >
+            Uitleg
+          </NuxtLink>
         </fwb-navbar-link>
-        <fwb-navbar-link
-          :is-active="$route.name === '/vote'"
-          link="/vote"
-        >
-          Stemmen
+
+        <fwb-navbar-link>
+          <NuxtLink
+            to="/vote"
+            active-class="text-TUcyan"
+          >
+            Stemmen
+          </NuxtLink>
         </fwb-navbar-link>
-        <fwb-navbar-link
-          :is-active="$route.name === '/license'"
-          link="/license"
-        >
-          Licentie
+
+        <fwb-navbar-link>
+          <NuxtLink
+            to="/license"
+            active-class="text-TUcyan"
+          >
+            Licentie
+          </NuxtLink>
         </fwb-navbar-link>
-        <fwb-navbar-link
-          :is-active="$route.name === '/contact'"
-          link="/contact"
-        >
-          Contact
+
+        <fwb-navbar-link>
+          <NuxtLink
+            to="/contact"
+            active-class="text-TUcyan"
+          >
+            Contact
+          </NuxtLink>
         </fwb-navbar-link>
       </fwb-navbar-collapse>
     </template>
--- components/OpeningNL.vue
+++ components/OpeningNL.vue
@@ -6,21 +6,21 @@ import { FwbButton, FwbP, FwbHeading } from 'flowbite-vue'
   <div class="p-5 max-w-prose text-justify">
     <FwbHeading
       tag="h5"
-      class="text-2xl font-bold tracking-tight text-gray-900"
+      class="mb-2 text-2xl font-bold tracking-tight text-gray-900"
     >
       Deelname aan onderzoek
     </FwbHeading>
-    <FwbP class="font-normal text-gray-700">
+    <FwbP>
       Beste deelnemer,
     </FwbP>
-    <FwbP class="font-normal text-gray-700">
+    <FwbP>
       U wordt uitgenodigd om deel te nemen aan een onderzoek genaamd
       <strong class="font-semibold text-gray-900">Contemporary Creativity: The Many Faces of AI Art.</strong>
       Dit onderzoek wordt uitgevoerd door Jari de Keijzer, Bachelor student Computer Science & Engineering aan de TU
       Delft, onder begeleiding van Dr. Anna Lukina.
     </FwbP>
 
-    <FwbP class="font-normal text-gray-700">
+    <FwbP>
       Het doel van dit onderzoek is om te ontdekken of afbeeldingen die gegenereerd worden door een specifiek
       Kunstmatige Intelligentie (AI) model creatief kunnen lijken. Om deze vraag te beantwoorden, stemt u, de
       deelnemer, op de creativiteit van paren van afbeeldingen. Voor elk paar afbeeldingen stemt u op welke afbeelding
@@ -32,8 +32,8 @@ import { FwbButton, FwbP, FwbHeading } from 'flowbite-vue'
       kost je ongeveer 15 minuten.
     </FwbP>
 
-    <FwbP class="font-normal text-gray-700">
-      Dit onderzoek is volledig anoniem, er worden dus GEEN persoonlijke gegevens verzameld. Deze stemwebsite wordt
+    <FwbP>
+      Dit onderzoek is <strong class="font-semibold text-gray-900">volledig anoniem</strong>, er worden dus GEEN persoonlijke gegevens verzameld. Deze stemwebsite wordt
       gehost op een server die eigendom is van de TU Delft, waarop geen andere gegevens dan uw anonieme stemmen worden
       verzameld. Deze stemmen worden gebruikt om de afbeeldingen te rangschikken op basis van een ELO-score (bekend
       van schaken). Met deze ranking wordt de creativiteit van het AI-model geëvalueerd. De verzamelde stemmen, de
@@ -41,63 +41,67 @@ import { FwbButton, FwbP, FwbHeading } from 'flowbite-vue'
       beschikbaar zal zijn op de TU Delft repository.
     </FwbP>
 
-    <FwbP class="font-normal text-gray-700">
+    <FwbP>
       Iedereen van <strong class="font-semibold text-gray-900">18 jaar of ouder</strong> mag meedoen aan dit
       onderzoek. Het is belangrijk voor dit onderzoek om een gevarieerde verzameling stemmen te krijgen. Meer
       deelnemers verhoogt de nauwkeurigheid van de ranglijst. Daarom is het u, de deelnemer, toegestaan om dit
       onderzoek met anderen te delen.
     </FwbP>
 
-    <FwbP class="font-normal text-gray-700">
+    <FwbP>
       Uw deelname aan dit onderzoek is geheel vrijwillig en u kunt zich op elk moment terugtrekken. Aangezien de
       stemmen volledig anoniem zijn, kunnen ze niet aan u worden gekoppeld, waardoor het onmogelijk is om 'uw' stemmen
       te verwijderen na het indienen.
     </FwbP>
 
-    <FwbP class="font-normal text-gray-700">
+    <FwbP>
       Doorgaan naar het volgende scherm start het stemproces, waardoor u een anonieme deelnemer bent.
     </FwbP>
 
-    <FwbP class="font-normal text-gray-700">
+    <FwbP>
       Eventuele vragen of opmerkingen kunnen naar het volgende e-mailadres worden gestuurd:
-      <a
-        href="mailto:j.n.dekeijzer@student.tudelft.nl?subject=Research Project"
+      <NuxtLink
+        to="mailto:j.n.dekeijzer@student.tudelft.nl?subject=Research Project"
         class="font-medium text-TUcyan underline hover:text-TUdarkblue hover:no-underline"
       >
         j.n.dekeijzer@student.tudelft.nl
-      </a>
+      </NuxtLink>
     </FwbP>
 
-    <FwbP class="font-normal text-gray-700">
+    <FwbP>
       Hartelijk dank voor uw tijd en deelname,
     </FwbP>
-    <FwbP class="font-normal text-gray-700">
+    <FwbP>
       Jari de Keijzer
     </FwbP>
 
-    <FwbButton
-      href="/explanation"
-      class="bg-TUcyan hover:bg-TUdarkblue focus:outline-none focus:ring-4 focus:ring-TUblue active:outline-none active:ring-4 active:ring-TUblue"
+    <NuxtLink
+      to="/explanation"
+      class="flex w-full"
     >
-      Ik begrijp het en doe mee aan het onderzoek
-      <svg
-        class="size-[24px] inline"
-        aria-hidden="true"
-        xmlns="http://www.w3.org/2000/svg"
-        width="24"
-        height="24"
-        fill="currentColor"
-        viewBox="0 0 24 24"
+      <FwbButton
+        class="w-full bg-TUcyan hover:bg-TUdarkblue focus:outline-none focus:ring-4 focus:ring-TUblue active:outline-none active:ring-4 active:ring-TUblue"
       >
-        <path
-          stroke="currentColor"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          stroke-width="2"
-          d="M19 12H5m14 0-4 4m4-4-4-4"
-        />
-      </svg>
-    </FwbButton>
+        Ik begrijp het en doe mee
+        <svg
+          class="size-[24px] inline"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          fill="currentColor"
+          viewBox="0 0 24 24"
+        >
+          <path
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M19 12H5m14 0-4 4m4-4-4-4"
+          />
+        </svg>
+      </FwbButton>
+    </NuxtLink>
   </div>
 </template>
 
